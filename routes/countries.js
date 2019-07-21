61d9307e1e7a3da33989a4c0265a639b43eb1f8c
fabio-malizia <fabio.malizia@itisriva.gov.it>
2019-07-21 21:43:40 +0200

    aggiunto send parametrico

--- routes/countries.js
+++ routes/countries.js
@@ -1,14 +1,9 @@
 var express = require('express'); //Carica il modulo express
-var router = express.Router(); //Otteine un oggetto router
+var router = express.Router(); //Ottiene un oggetto router
 var country = require('countryjs');
 
 
-router.get('/info/US', function(req, res, next) {
-  res.send(country.info('US'));
-});
-
-router.get('/info/IT', function(req, res, next) {
-  res.send(country.info('IT'));
-});
-
+router.get('/info/:state/', function(req, res, next){
+  res.send(country.info(req.params.state)); //Rendiamo lo stato un parametro
+})
 module.exports = router; //esporta il modulo per poterlo usare in app.js
